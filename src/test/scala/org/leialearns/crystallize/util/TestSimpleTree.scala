72919c0f057d65186c42d7818ef3c820dfe365e3
Jeroen van Maanen <jeroen@sollunae.net>
2018-03-28 11:36:26 +0200

    Fixed warnings in class TestSimpleTree.

--- src/test/scala/org/leialearns/crystallize/util/TestSimpleTree.scala
+++ src/test/scala/org/leialearns/crystallize/util/TestSimpleTree.scala
@@ -4,38 +4,37 @@ import org.scalatest.FunSuite
 
 import grizzled.slf4j.Logging
 import org.leialearns.crystallize.util.Rational._
-import scala.math._
-import scala.math.Numeric
 
 class TestSimpleTree extends FunSuite with LoggingConfiguration with Logging {
+  private val testLimit = 300
 
   test("SimpleTree") {
     def comparator(value: OrderedRational, item: Rational): Int = {
       RationalIsFractional.compare(value.r, item)
     }
-    def withinBounds(limit: Int)(value: OrderedRational) = value.limit < limit
+    def withinBounds(limit: Int)(value: OrderedRational): Boolean = value.limit < limit
 
     val tree = Oracle.oracle
     val unity = Rational(1, 1)
     val half = Rational(1, 2)
     var r = Rational(0, 1)
     while (r < unity) {
-      debug(s"Rational: ${r}")
-      val value = tree.next(r, comparator, withinBounds(300)).get
-      assert(value.limit < 300)
+      debug(msg = s"Rational: ${r}")
+      val value = tree.next(r, comparator, withinBounds(testLimit)).get
+      assert(value.limit < testLimit)
       if (r.numerator > 0) {
-        val back = tree.previous(value.r, comparator, withinBounds(300)).get
+        val back = tree.previous(value.r, comparator, withinBounds(testLimit)).get
         assert(back.r == r)
         val v: Rational = value.r
         val m = (value.r + r) * half
-        val bracket = tree.getBracket(m, comparator, withinBounds(300))
-        debug(s"Bracket: ${m}: ${bracket}")
+        val bracket = tree.getBracket(m, comparator, withinBounds(testLimit))
+        debug(msg = s"Bracket: ${m}: ${bracket}")
         bracket match {
           case (Some(left), Some(right)) => {
             assert(left.r == r)
             assert(right.r == v)
           }
-          case _ => assert(false)
+          case _ => assert(condition = false)
         }
       }
       r = value.r
