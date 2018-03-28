04646f273eed73e7e2a55b19c417683c68442ee3
Jeroen van Maanen <jeroen@sollunae.net>
2018-03-28 11:48:31 +0200

    Factored out method Optimizer#optimize from Optimizer#consumeEvent.

--- src/main/scala/org/leialearns/crystallize/expectations/Optimizer.scala
+++ src/main/scala/org/leialearns/crystallize/expectations/Optimizer.scala
@@ -1,10 +1,12 @@
 package org.leialearns.crystallize.expectations
 
-import grizzled.slf4j.Logging
-import org.leialearns.crystallize.event._
-import org.leialearns.crystallize.event.History._
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.atomic.AtomicLong
+
+import grizzled.slf4j.Logging
+import org.leialearns.crystallize.event.History._
+import org.leialearns.crystallize.event._
+import org.leialearns.crystallize.item.Item
 
 class Optimizer(val history: History) extends Logging {
   val thread = new Thread(() => optimizer())
@@ -38,16 +40,20 @@ class Optimizer(val history: History) extends Logging {
     val eventHandle = eventQueue.take()
     queueSize.decrementAndGet()
     eventHandle.event match {
-      case ObservedEvent(node, item) =>
+      case ObservedEvent(state, item) =>
         val snapshot = history.lastSnapshot
         val current: Model = snapshot._1
-        if (getNodeValue(current, node) forall (_.updatedUpTo < eventHandle.ordinal)) {
-          debug(s"Optimize: ${eventHandle.ordinal}: ${item}: ${node}")
-          val event = ExpectedNodeVerifiedEvent(node, snapshot._2.ordinal)
+        if (getNodeValue(current, state) forall (_.updatedUpTo < eventHandle.ordinal)) {
+          debug(s"Optimize: ${eventHandle.ordinal}: ${item}: ${state}")
+          val event = optimize(snapshot, eventHandle, state, item)
           history.addEvent(event)
           Thread.sleep(100)
         }
       case _ => ()
     }
+  }
+
+  def optimize(snapshot: Snapshot, eventHandle: EventHandle[State], state: State, item: Item): Event[State] = {
+    ExpectedNodeVerifiedEvent(state, snapshot._2.ordinal)
   }
 }
