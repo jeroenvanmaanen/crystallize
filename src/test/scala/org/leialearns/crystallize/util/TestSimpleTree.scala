59f1047f8ef404c5a5317ee0b556a45fba40472e
Jeroen van Maanen <jeroen@sollunae.net>
2018-03-28 12:19:08 +0200

    Replaced assert(false) by fail() in TestSimpleTree.

--- src/test/scala/org/leialearns/crystallize/util/TestSimpleTree.scala
+++ src/test/scala/org/leialearns/crystallize/util/TestSimpleTree.scala
@@ -5,7 +5,6 @@ import org.scalatest.FunSuite
 import grizzled.slf4j.Logging
 import org.leialearns.crystallize.util.Rational._
 
-//noinspection NameBooleanParameters
 class TestSimpleTree extends FunSuite with LoggingConfiguration with Logging {
   private val testLimit = 300
 
@@ -35,7 +34,7 @@ class TestSimpleTree extends FunSuite with LoggingConfiguration with Logging {
             assert(left.r == r)
             assert(right.r == v)
           }
-          case _ => assert(false)
+          case _ => fail()
         }
       }
       r = value.r
